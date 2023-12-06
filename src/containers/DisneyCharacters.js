6bd81c65d1b74d6ef7d1c7a7889e8494fc75e476
Rayster Ramos <rjrfrst@gmail.com>
2023-12-06 16:31:46 +0000

    Able to fetch the API

--- src/containers/DisneyCharacters.js
+++ src/containers/DisneyCharacters.js
@@ -1,5 +1,25 @@
 import CharactersList from "../components/CharactersList";
+import { useState, useEffect } from "react";
 const DisneyCharacters = () => {
+
+    // Create estates
+    const [disneyCharacters, setDisneyCharacters] = useState(null);
+
+    // Fetch the API 
+        const fetchDisneyApi = async () => {
+            const response = await fetch("https://api.disneyapi.dev/character")
+            const data = await response.json();
+            setDisneyCharacters(data);
+            console.log(disneyCharacters);
+        }
+
+    // 
+        useEffect(() => {
+            fetchDisneyApi();
+        },[])
+
+
+
     return ( 
         <>
         <h1>This is the container</h1>
